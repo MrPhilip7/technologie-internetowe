1c10e98ca2ad5b828f66bc27c73dae6c7b95bfce
Filip Czerwiński <filipczer@interia.pl>
2026-02-25 09:31:47 +0100

    Add Category and Film models; add EF Core refs
    Add new Category and Film model classes (Category includes Id, Name, Description and a Films collection; Film is scaffolded empty). Remove ErrorViewModel, the Error view, the validation scripts partial, and the Error action from HomeController. Update the project file to add Microsoft.EntityFrameworkCore, SqlServer and Tools package references and include the Models folder.

---
+++ technologieInternetowe/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace technologieInternetowe.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Film> Films { get; set; }
+    }
+}
---
+++ technologieInternetowe/Models/Film.cs
@@ -0,0 +1,6 @@
+﻿namespace technologieInternetowe.Models
+{
+    public class Film
+    {
+    }
+}
